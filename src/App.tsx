0cbbea89565667e2a30a45bfccbc7f064a3bbe0e
francodealesandro <Francodealesandro@gmail.com>
2023-01-30 14:30:40 -0300

    wakelook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// The wake lock sentinel.
+let wakeLock = null;
+
+// Function that attempts to request a wake lock.
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    wakeLock.addEventListener('release', () => {
+      console.log('Wake Lock was released');
+    });
+    console.log('Wake Lock is active');
+  } catch {
+    console.log('Wake Lock error');
+  }
+};
+
 function App() {
+  requestWakeLock();
+
   return (
     <div className="App">
       <header className="App-header">
